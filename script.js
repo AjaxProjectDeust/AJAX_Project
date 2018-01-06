6b9f84f3a5faed2f72ea8036a33b654a0c03bacd
Tony Bengué <tonybengue@hotmail.fr>
2018-01-06 19:16:44 +0100

    js

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 //Insérer le Jquery dans entre les accolades
+/*
 $(document).ready(function() {
-  
-  
+ $(div).innerHTML = "salut";   
 });
+*/
+
+   
+        var tag = document.getElementById("div1").innerHTML; 
+        alert(tag);
+        
