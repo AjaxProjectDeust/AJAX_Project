711736348fd0a33515ee802c09d942065f896c1f
Tony Bengué <tonybengue@hotmail.fr>
2018-01-09 13:49:15 +0100

    maj js

--- script.js
+++ script.js
@@ -1,21 +1,72 @@
-//Insérer le Jquery dans entre les accolades
-/*
+/*saisie nbre de face
+random en fonction du nombre de faces 
+chaque joueur lance 3 dés
+meilleure combinaison, donne des jetons à l'autre en fonction de la combinaison
+Si combinaison equivalente, on relance le tour.
+Si joueur n'a plus de jetons => perdu
+*/
 
-var faces = prompt('Entrez le nombre de faces souhaitées : ');
+//attribut onclick devient la propriété onclick
+h1.onclick = function(){
+  this.innerHTML="<strong>Salut</strong>";
+  this.style.color="orange";
+};
 
-function lance_de(minNbrFaces,faces){
-    return alert(faces);
+//joueur 1
+var player_1 = 0;
+//joueur 2
+var player_2 = 0;
+
+//// On crée nos différents "écouteurs" d'événements
+var button = document.getElementById("button");
+var click = button.addEventListener('click', demande_face);
+
+//id du tag
+var ret_1 = document.getElementById("div1");
+var ret_2 = document.getElementById("div2");
+var ret_3 = document.getElementById("div3");
+//var h1 = document.getElementById("h1");
+
+function demande_face(){
+    //Demande le nombre de face du dé
+    var nbre_faces = prompt('Entrez le nombre de faces du dé souhaité : ');
+    //appelle la fonction lance_de avec le nombre de faces voulu
+    lance(nbre_faces);
 }
 
-
-function lance_3d(minNbrFaces,pNbrFaces) {
+//lance  de
+var lance = function lance_de(nbre_faces){
+   // entre 1 et le le nombre de faces du dé
+   var random_faces = Math.floor(Math.random() * nbre_faces + 1);
+   //affiche dans la console
+    ret_1.textContent = "Le premier tirage est : " + random_faces;
+    console.log("Le nombre aléatoire est " + random_faces);
+   
+    var reponse = prompt("Voulez vous relancer automatiquement? \nRéponse oui ou non");
     
-    for (var i = 0; i <= 3; i++) {
-       /*lance 3 fois entre minNbrFaces soit 1 ,pNbrFaces*/ 
-
-    
+    if(reponse === "oui"){
+        lance_3d(random_faces)
+    }else if(reponse === "non"){
+        var reponse_manuelle = prompt("Manuellement ? \nRéponse oui ou non");
+        if (reponse_manuelle === "oui"){
+            alert("");
+        }else if( reponse ==="non" ){
+              alert("Tampis");
+        }else{
+              alert("Veuillez recharger la page");
+        }
+    }else{
+        alert("Mauvaise synthaxe");
     }
-    
+}
+//lance 3 dés
+function lance_3d(nbre_faces) {
+    for (var i = 0; i < 2; i++) {
+        var random_faces = Math.floor(Math.random() * nbre_faces + 1);
+        console.log(random_faces);
+        //affiche dans la console
+        ret_2.textContent = "\nLe deuxième tirage est : " + random_faces;
+        ret_3.textContent = "\nLe troisième tirage est : " + random_faces;
+    }
 }
 
-var tag = document.getElementById("div1").innerHTML;
